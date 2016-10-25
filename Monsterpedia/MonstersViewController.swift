df1f43b8c296f627b7d4789fa99fd6307d37f47d
Eman <emmanuoel.h@gmail.com>
2016-10-25 02:02:30 -0700

    Resign first responder after cell selection

--- Monsterpedia/MonstersViewController.swift
+++ Monsterpedia/MonstersViewController.swift
@@ -89,6 +89,10 @@ extension MonstersViewController: UITableViewDelegate, UITableViewDataSource {
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		tableView.deselectRow(at: indexPath, animated: true)
+		if searchBar.isFirstResponder {
+			searchBar.resignFirstResponder()
+		}
+		
 		let cell = tableView.cellForRow(at: indexPath) as! MonsterSpriteCell
 		selectedMonster = filteredMonsters[indexPath.row]
 		if isTeamBuilding {
