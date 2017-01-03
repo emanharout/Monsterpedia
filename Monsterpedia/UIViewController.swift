a674e89437232ca597c01a8aa03f32a75210a5a9
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-01-02 22:16:58 -0800

    Create DexSelectionViewController

---
+++ Monsterpedia/UIViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController.swift
+//  Monsterpedia
+//
+//  Created by Emmanuoel Haroutunian on 1/2/17.
+//  Copyright © 2017 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class DexSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
