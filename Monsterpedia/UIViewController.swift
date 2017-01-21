d7ed801f8bbbe713cf64c0269169f29eed21264a
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-01-20 22:41:11 -0800

    Create DexSelectionVC protocol and call delegate’s function

--- Monsterpedia/UIViewController.swift
+++ Monsterpedia/UIViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class DexSelectionViewController: UIViewController {
+  
+  var delegate: DexSelectionViewControllerDelegate?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
+
+  @IBAction func selectDex(_ sender: UIButton) {
+    switch sender.tag {
+    case 0:
+      delegate?.userDidSelect(Dex.kanto)
+    case 1:
+      delegate?.userDidSelect(Dex.johto)
+    case 2:
+      delegate?.userDidSelect(Dex.hoenn)
+    case 3:
+      delegate?.userDidSelect(Dex.sinnoh)
+    case 4:
+      delegate?.userDidSelect(Dex.unova)
+    case 5:
+      delegate?.userDidSelect(Dex.kalos)
+    default:
+      print("Error sending Dex to DexSelectionViewController's delegate")
     }
+  }
+}
 
+protocol DexSelectionViewControllerDelegate {
+  func userDidSelect(_ dex: Dex)
 }
