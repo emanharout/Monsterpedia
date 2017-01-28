3250859761ad10d9290c6854080b935d86c1cf47
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-01-28 01:22:28 -0800

    Extend ImageView to add vertical constraint adjustment method

--- Monsterpedia/UIImageView+DynamicHeight.swift
+++ Monsterpedia/UIImageView+DynamicHeight.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2017 Emmanuoel Haroutunian. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+  func adjust(vertical constraint: NSLayoutConstraint, toFit image: UIImage) {
+    let imageViewWidth = self.frame.width
+    let imageWidth = image.size.width
+    let imageHeight = image.size.height
+    let scaleRatio = imageViewWidth / imageWidth
+    
+    
+    let adjustedConstraintConstant = scaleRatio * imageHeight
+    print("Constant: \(adjustedConstraintConstant)")
+    constraint.constant = adjustedConstraintConstant
+  }
+}
